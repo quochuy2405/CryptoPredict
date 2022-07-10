124920625d3bf676cf3d185fe9ed6cbaa4ee5986
quochuy2405 <huy01682312920@gmail.com>
2022-07-10 22:31:24 +0700

    use chart

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Admin, Page404, Test } from './page'
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/" element={<Admin />} />
+        <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
+        <Route path="/admin/:slug" element={<Admin />} />
+        <Route path="/admin/:slug/:opt" element={<Admin />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/index.ts
@@ -0,0 +1,3 @@
+export { default as Admin } from './Admin'
+export { default as Test } from './Test'
+export { default as Page404 } from './Page404'
